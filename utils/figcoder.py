e19bf024bbf7162a36cdc605b86b21b4aeada074
lee <lee_jiazh@163.com>
2018-11-14 13:49:02 +0800

    20181114

--- utils/figcoder.py
+++ utils/figcoder.py
@@ -8,17 +8,21 @@ __author__ = "Gajanlee"
 
 parse = argparse.ArgumentParser()
 parse.add_argument("--figs", help="figs' path", nargs="+", required=True)
+parse.add_argument("--prefix", help="generate tag name's prefix", default="pic", type=str)
 
 def code_fig(path):
+    print(path)
     with open(path, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-def output(data, path="pic_base64.txt"):
+def output(data, path="pic_base64.txt", prefix="pic"):
+    print(prefix, len(data))
     with open(path, "w") as f:
         for i, code in enumerate(data, 1):
-            f.writelines("[pic{num}]:data:image/png;base64,{code}".format(num=i, code=code))
+            f.write("[{prefix}{num}]:data:image/png;base64,{code}\n".format(prefix=prefix, num=i, code=code))
 
 if __name__ == "__main__":
-    output([code_fig(file) for file in parse.parse_args().figs])
+    args = parse.parse_args()
+    output([code_fig(file) for file in args.figs], prefix=args.prefix)
 
 
